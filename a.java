872c63837cdc67c8488c857f4b66a8d782af7b93
PalakAtGitHub <aggarwalpalak25062gmail.com>
2022-10-28 21:40:41 +0530

    Linked list common elements

--- a.java
+++ a.java
@@ -1,15 +1,37 @@
 class A
 {
      public static void main (String[] args) {
-       for (File subfile : file.listFiles()) {
+       ArrayList<String>
+            list1 = new ArrayList<String>();
   
-            // if it is a subfolder,e.g Rohan and Ritik,
-            // recursiley call function to empty subfolder
-            if (subfile.isDirectory()) {
-                deleteDirectory(subfile);
-            }
+        // Add values in ArrayList
+        list1.add("Hii");
+        list1.add("Geeks");
+        list1.add("for");
+        list1.add("Geeks");
   
-            // delete files and empty subfolders
-            subfile.delete();
-      }
-      }
+        // print list 1
+        System.out.println("List1: "
+                           + list1);
+  
+        // Create ArrayList list2
+        ArrayList<String>
+            list2 = new ArrayList<String>();
+  
+        // Add values in ArrayList
+        list2.add("Hii");
+        list2.add("Geeks");
+        list2.add("Gaurav");
+  
+        // print list 2
+        System.out.println("List2: "
+                           + list2);
+  
+        // Find the common elements
+        list1.retainAll(list2);
+  
+        // print list 1
+        System.out.println("Common elements: "
+                           + list1);
+    }
+}
