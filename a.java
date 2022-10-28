8bb972e05c3ced3eaa575917bfb8db4b3c1de54d
PalakAtGitHub <aggarwalpalak25062gmail.com>
2022-10-28 21:11:38 +0530

    Java 2

--- a.java
+++ a.java
@@ -1,11 +1,11 @@
 class A
 {
     System.out.println("Hello World");
-     int a=5;
+      int a=5;
       int b=0;
         try{
           System.out.println(a/b);
         }
       catch(ArithmeticException e){
-        e.printStackTrace();
-}
+        System.out.println(e.toString());
+      }
