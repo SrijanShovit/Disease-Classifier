a7e6aa749f3b64ca655597e5de8e1e535ad6a23b
PalakAtGitHub <aggarwalpalak25062gmail.com>
2022-10-28 21:59:11 +0530

    JAVA

--- a.java
+++ a.java
@@ -1,6 +1,7 @@
 class GeeksforGeeks {
-    private static void main(String[] args)
+    public static int main(String[] args)
     {
         System.out.println("I am a Geek");
+        return 1;
     }
 }
