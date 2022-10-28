74affe4f7feef3136d8f1dc1c0968ba615ba8de5
PalakAtGitHub <aggarwalpalak25062gmail.com>
2022-10-28 21:55:17 +0530

    Java LL

--- a.java
+++ a.java
@@ -12,42 +12,36 @@ public class GFG {
 	{
 
 		// create ArrayList list1
-		ArrayList<String>
-			list1 = new ArrayList<String>();
-
-		// Add values in ArrayList
-		list1.add("Hii");
-		list1.add("Geeks");
-		list1.add("for");
-		list1.add("Geeks");
-
-		// print list 1
-		System.out.println("List1: "
-						+ list1);
-
-		// Create ArrayList list2
-		ArrayList<String>
-			list2 = new ArrayList<String>();
-
-		// Add values in ArrayList
-		list2.add("Hii");
-		list2.add("Geeks");
-		list2.add("Gaurav");
-
-		// print list 2
-		System.out.println("List2: "
-						+ list2);
-
-		// Create ArrayList list3
-		ArrayList<String>
-			list3 = new ArrayList<String>(list1);
-
-		// Store the comparison output
-		// in ArrayList list3
-		list3.retainAll(list2);
-
-		// print list 3
-		System.out.println("Common elements: "
-						+ list3);
+		 ArrayList<String>
+            list1 = new ArrayList<String>();
+  
+        // Add values in ArrayList
+        list1.add("Hii");
+        list1.add("Geeks");
+        list1.add("for");
+        list1.add("Geeks");
+  
+        // print list 1
+        System.out.println("List1: "
+                           + list1);
+  
+        // Create ArrayList list2
+        ArrayList<String>
+            list2 = new ArrayList<String>();
+  
+        // Add values in ArrayList
+        list2.add("Hii");
+        list2.add("Geeks");
+        list2.add("Gaurav");
+  
+        // print list 2
+        System.out.println("List2: "
+                           + list2);
+  
+        // Find common elements
+        System.out.print("Common elements: ");
+        System.out.println(list1.stream()
+                               .filter(list2::contains)
+                               .collect(Collectors.toList()));
 	}
 }
