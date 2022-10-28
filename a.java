fc94909a1def1ae14c8771c47b0f0f1c3ee2808e
PalakAtGitHub <aggarwalpalak25062gmail.com>
2022-10-28 21:21:09 +0530

    Java 4

--- a.java
+++ a.java
@@ -1,12 +1,15 @@
 class A
 {
      public static void main (String[] args) {
-      int a=5;
-      int b=0;
-        try{
-          System.out.println(a/b);
-        }
-      catch(ArithmeticException e){
-        System.out.println(e.getMessage());
+       for (File subfile : file.listFiles()) {
+  
+            // if it is a subfolder,e.g Rohan and Ritik,
+            // recursiley call function to empty subfolder
+            if (subfile.isDirectory()) {
+                deleteDirectory(subfile);
+            }
+  
+            // delete files and empty subfolders
+            subfile.delete();
       }
       }
