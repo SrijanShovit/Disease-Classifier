fc1f61d51d419eccd9be1a7a01929e92dde292b3
PalakAtGitHub <aggarwalpalak25062gmail.com>
2022-10-28 21:10:00 +0530

    Arithmetic exception

--- a.java
+++ a.java
@@ -1,4 +1,11 @@
 class A
 {
     System.out.println("Hello World");
+     int a=5;
+      int b=0;
+        try{
+          System.out.println(a/b);
+        }
+      catch(ArithmeticException e){
+        e.printStackTrace();
 }
