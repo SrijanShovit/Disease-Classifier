3c14a25472ec615f8882bb485e928ad305b36705
PalakAtGitHub <aggarwalpalak25062gmail.com>
2022-10-28 22:45:12 +0530

    Node

--- a.java
+++ a.java
@@ -1,22 +1,29 @@
-
-// Given a reference (pointer to pointer)
-// to the head of a list and an int,
-// inserts a new node on the front of
-// the list.
-void push(Node** head_ref, int new_data)
+// Given a node prev_node, insert a
+// new node after the given
+// prev_node
+void insertAfter(Node* prev_node, int new_data)
 {
-   
-    // 1. allocate node
-    Node* new_node = new Node();
- 
-    // 2. put in the data
-    new_node->data = new_data;
- 
-    // 3. Make next of new node as head
-    new_node->next = (*head_ref);
- 
-    // 4. Move the head to point to
-    // the new node
-    (*head_ref) = new_node;
+
+	// 1. Check if the given prev_node is NULL
+	if (prev_node == NULL) {
+		cout << "The given previous node cannot be NULL";
+		return;
+	}
+
+	// 2. Allocate new node
+	Node* new_node = new Node();
+
+	// 3. Put in the data
+	new_node->data = new_data;
+
+	// 4. Make next of new node as
+	// next of prev_node
+	new_node->next = prev_node->next;
+
+	// 5. move the next of prev_node
+	// as new_node
+	prev_node->next = new_node;
 }
- 
+
+// This code is contributed by anmolgautam818,
+// arkajyotibasak
