9d76e200cf72c4ef48a9658b97def1325d441b05
PalakAtGitHub <aggarwalpalak25062gmail.com>
2022-10-29 13:03:22 +0530

    lkjhdjd

--- a.java
+++ a.java
@@ -63,7 +63,7 @@ int main()
 	// 4 5 6 7
 	Node* root = newNode(1);
 	root->left = newNode(2);
-	root->right = newNode(3);
+	root->right  = newNode(3);
 	root->left->left = newNode(4);
 	root->left->right = newNode(5);
 	root->right->left = newNode(6);
