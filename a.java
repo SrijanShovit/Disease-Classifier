b3ed2d9b801149370c81a9f49901564b17066813
PalakAtGitHub <aggarwalpalak25062gmail.com>
2022-10-29 11:08:35 +0530

    LP

--- a.java
+++ a.java
@@ -64,10 +64,11 @@ int main()
 	Node* root = newNode(1);
 	root->left = newNode(2);
 	root->right = newNode(3);
-	root->left->left = newNode(4);
+	root->left->left =  newNode(4);
 	root->left->right = newNode(5);
 	root->right->left = newNode(6);
 	root->right->right = newNode(7);
+	
 	
 	
 	Node* next = NULL;
