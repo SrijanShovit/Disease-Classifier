765f5d9cc1005a1fbdb3f5dbd5128f60e2cb9f05
PalakAtGitHub <aggarwalpalak25062gmail.com>
2022-10-29 16:53:28 +0530

    hbdwd

--- a.java
+++ a.java
@@ -64,7 +64,7 @@ int main()
 	root->left->left = newNode(4);
 	root->left->right = newNode(5);
 	root->right->left = newNode(6);
-	root->right->right = newNode(7);
+	root->right->right =  newNode(7);
 	
 
 	Node* next =  NULL; 
