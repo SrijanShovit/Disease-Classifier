0906d9e5448742875638dd4741bed3dcfbf83b2e
PalakAtGitHub <aggarwalpalak25062gmail.com>
2022-10-31 18:15:35 +0530

    nhdnkj

--- a.java
+++ a.java
@@ -65,8 +65,8 @@ int main()
 	root->right->left = newNode(6) ;
 	root->right->right = newNode(7) ;
 
-	Node* next =  NULL; 
-	inorderSuccessor(root,root, next)  ;
+	Node* next =  NULL;
+	inorderSuccessor(root,root, next) ;
 	
 	next = NULL;
 	inorderSuccessor(root, root->left->left, next);
